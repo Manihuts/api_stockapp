8a2658b4a40bd61589d37a3303a3c8c03c9cd174
Philippe <philippedsantos@hotmail.com>
2024-11-22 18:50:00 -0300

    fix 1 fetchEvolucaoSaldo

--- server/routes/ativos.routes.js
+++ server/routes/ativos.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/fetch", fetchAtivos);                      // Busca informações de todos os ativos
 router.get("/fetch/:id", fetchUserAtivos);              // Busca ativos de um usuário
-router.get("fetch/evolution/:id", fetchEvolucaoSaldo);  // Busca a evolução do saldo de um usuário
+router.get("/fetch/evolution/:id", fetchEvolucaoSaldo);  // Busca a evolução do saldo de um usuário
 router.post("/buy", processaCompra);                    // Processa a compra de um ativo
 router.post("/sell", processaVenda);                    // Processa a venda de um ativo
 router.put("/fav/:id", processaFavorito);               // Favorita ou desfavorita um ativo
